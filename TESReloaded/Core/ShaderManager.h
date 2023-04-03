eb529352c6ecb03704426b06fb74f1e2a48d90d4
Mcstfuerson <Mcstfuerson>
2023-04-03 01:08:52 -0400

    added un-init state

--- TESReloaded/Core/ShaderManager.h
+++ TESReloaded/Core/ShaderManager.h
@@ -46,6 +46,7 @@ enum DayPhase
 
 enum CellLocation
 {
+	Unk,
 	Interior,
 	Exterior,
 	Fake_Exterior
@@ -280,7 +281,6 @@ public:
 
 	void					CreateCT();
 	void					SetCT();
-	void					SetCustomCT();
 	bool					LoadShader(const char* Name, const char* DirPostFix = "");
 	
 	ShaderType				Type;
