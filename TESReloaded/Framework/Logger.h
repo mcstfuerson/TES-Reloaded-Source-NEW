5b235a7f84909c821de163edecde279c52151e39
Mcstfuerson <Mcstfuerson>
2023-12-22 09:53:37 -0500

    increase log message buffer size

--- TESReloaded/Framework/Logger.h
+++ TESReloaded/Framework/Logger.h
@@ -5,7 +5,7 @@ public:
 	static void CreateLog(char* FileName);
 	static void Log(char* Message, ...);
 	
-	static char			MessageBuffer[1024];
+	static char			MessageBuffer[2048];
 	static FILE*		LogFile;
 
 };
